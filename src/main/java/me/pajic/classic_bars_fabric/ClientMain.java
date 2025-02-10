a85723ecec71de1993832bc55341b85dfb9cfdfe
pajicadvance <pavle.pajic23@gmail.com>
2025-02-10 12:53:34 +0100

    Prepare dev env

---
+++ src/main/java/me/pajic/classic_bars_fabric/ClientMain.java
@@ -0,0 +1,10 @@
+package me.pajic.classic_bars_fabric;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ClientMain implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/me/pajic/classic_bars_fabric/Main.java
@@ -0,0 +1,10 @@
+package me.pajic.classic_bars_fabric;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Main implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
